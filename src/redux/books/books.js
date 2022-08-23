0475b331923edbe3c368d838d29ec4bae3059392
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-08-23 03:00:51 +0300

    Update action type definition for books and catagories with ducks pattern

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,20 +1,24 @@
 // ACTION
+const ADD_BOOK = 'bookstore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+
+// ACTION CREATORS
 export const addBook = (book) => ({
-  type: 'ADD_BOOK',
+  type: ADD_BOOK,
   data: book,
 });
 
 export const removeBook = (id) => ({
-  type: 'REMOVE_BOOK',
+  type: REMOVE_BOOK,
   data: id,
 });
 
 // REDUCER
 const bookReducer = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_BOOK':
+    case ADD_BOOK:
       return [...state, action.data];
-    case 'REMOVE_BOOK':
+    case REMOVE_BOOK:
       return state.filter((books) => books.id !== action.data);
     default:
       return state;
